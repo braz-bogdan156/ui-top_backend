fddafaf69a051f914508fb5139a679e4f06dfca0
Богдан <brazbogdan@gmail.com>
2025-10-22 17:56:57 +0300

    updated_main

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONTEND_URL || process.env.EXTERNAL_FRONTEND_URL,
   });
   await app.listen(PORT, () => {
     console.log(`Application is running on: http://localhost:${PORT}`);
