c4992875219dd7e48a07dac9b79c903edba992a2
Богдан <brazbogdan@gmail.com>
2025-10-31 14:07:01 +0200

    updated_main

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'https://ui-top-frontend.vercel.app',
+  ];
+
   app.enableCors({
-    origin: process.env.EXTERNAL_FRONTEND_URL,
+    origin: allowedOrigins,
+    credentials: true, // якщо використовуєш куки або авторизацію
   });
+
   await app.listen(PORT, () => {
     console.log(`Application is running on: http://localhost:${PORT}`);
   });
